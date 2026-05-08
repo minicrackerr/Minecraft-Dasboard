86fbe9173b02be2728b23dc7e613ffd3cd366d7c
minicrackerr <168635872+minicrackerr@users.noreply.github.com>
2026-05-08 03:29:39 +0200

    bug fixxes

--- functionality/server_status.py
+++ functionality/server_status.py
@@ -59,7 +59,7 @@ class Server:
             chat_history = new_message = ""
             for line in file:
                 if not line.find("[Not Secure]") == -1:
-                    message = f'[{line[12:17]}] {line.replace("[Server thread/INFO] [net.minecraft.server.MinecraftServer/]: [Not Secure]", "").replace("[","<").replace("]",">").replace("<Rcon>","<Dasboard>")[27:]}'
+                    message = f'[{line[11:16]}] {line.replace("[Server thread/INFO] [net.minecraft.server.MinecraftServer/]: [Not Secure]", "").replace("[","<").replace("]",">").replace("<Rcon>","<Dashboard>")[25:]}'
                     #\n each BP (BREAKING_POINT) chars
                     BP = 52
                     if len(message) > BP:
--- main.py
+++ main.py
@@ -50,14 +50,14 @@ def controls_triggered():
     elif button_value == "restart":
         playit_launcher.restart_instance()
         server.launcher.restart_instance()
-    return redirect("/")
+    return redirect(f"/{server_list[server].name}")
 
 @app.route("/command_submitted", methods=["POST"])
 def command_submitted():
     text_input = request.form.get("command")
     # print(request.headers.get())
     server.send_message(text_input)
-    return redirect("/")
+    return redirect(f"/{server_list[server].name}")
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0",port=80)
