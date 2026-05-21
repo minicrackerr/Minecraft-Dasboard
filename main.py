bb6ae2232d671d2a4225b5034986aacd7f597763
minicrackerr <168635872+minicrackerr@users.noreply.github.com>
2026-05-21 12:58:27 +0200

    download fixxxed

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def server_controlls(server_name, value):
 ### FILE ACCESS
 
 @app.route('/server/<server_name>/modpack_download')
-def return_modpack_download(server_name, filename):
+def return_modpack_download(server_name):
     modpack_path = f"server/{server_list[server_name].name}/"
     return flask.send_from_directory(modpack_path, "modpack_download.zip")
 
